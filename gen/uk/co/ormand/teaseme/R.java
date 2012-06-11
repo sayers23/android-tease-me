697b88dd7a7b1aaa593f2a20ba43c89bb3a067ff
philormand <philormand@hotmail.com>
2012-06-11 20:38:40 +0000

    Minor Tweaks

--- gen/uk/co/ormand/teaseme/R.java
+++ gen/uk/co/ormand/teaseme/R.java
@@ -14,27 +14,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LayoutImage=0x7f070001;
-        public static final int LayoutRight=0x7f070002;
-        public static final int LayoutTextTop=0x7f070003;
-        public static final int LayoutTop=0x7f070000;
-        public static final int btnLayout=0x7f070007;
-        public static final int btnPasswordOk=0x7f070009;
-        public static final int editTextPassword=0x7f070008;
-        public static final int settings_text_view=0x7f07000a;
-        public static final int textView1=0x7f070004;
-        public static final int textViewDebug=0x7f070005;
-        public static final int webView1=0x7f070006;
+        public static final int LayoutImage=0x7f070002;
+        public static final int LayoutRight=0x7f070003;
+        public static final int LayoutTextTop=0x7f070004;
+        public static final int LayoutTop=0x7f070001;
+        public static final int btnLayout=0x7f070009;
+        public static final int btnPasswordOk=0x7f07000b;
+        public static final int digitalClock1=0x7f070007;
+        public static final int editTextPassword=0x7f07000a;
+        public static final int settings_text_view=0x7f07000c;
+        public static final int textViewDebug=0x7f070006;
+        public static final int textViewTimer=0x7f070005;
+        public static final int webView1=0x7f070008;
+        public static final int webViewAbout=0x7f070000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int passwordentry=0x7f030001;
-        public static final int show_settings_layout=0x7f030002;
+        public static final int about=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int passwordentry=0x7f030002;
+        public static final int show_settings_layout=0x7f030003;
     }
     public static final class raw {
-        public static final int android=0x7f050000;
-        public static final int atm=0x7f050001;
-        public static final int tick=0x7f050002;
+        public static final int tick=0x7f050000;
     }
     public static final class string {
         public static final int Image=0x7f060001;
